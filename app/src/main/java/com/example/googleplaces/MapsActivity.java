d63c37cf7ea27e55728d790cd16f7f78af5b2f78
moseskamira <42971167+moseskamira@users.noreply.github.com>
2020-08-10 11:35:45 +0300

    Create README.md

--- app/src/main/java/com/example/googleplaces/MapsActivity.java
+++ app/src/main/java/com/example/googleplaces/MapsActivity.java
@@ -1,47 +1,296 @@
 package com.example.googleplaces;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap myGoogleMap;
+    private PlacesClient placesClient;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private MaterialSearchBar materialSearchBar;
+    private Location lastKnownLocation;
+    private LocationCallback locationCallback;
+    private static final int LOC_PERMISSION_REQ_CODE = 23;
+    private static final int LOC_ENABLED_CHECK_CODE = 24;
+    private float DEFAULT_ZOOM = 18;
+    private LocationRequest locationRequest;
+    private AutocompleteSessionToken autocompleteSessionToken;
+
+    private List<String> suggestionsList;
+    private Button submitBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        materialSearchBar = findViewById(R.id.search_bar);
+        submitBtn = findViewById(R.id.submit_location_tbn);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+        String api_key = getString(R.string.goole_api_key);
 
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), api_key);
+        }
+        placesClient = Places.createClient(this);
+        autocompleteSessionToken = AutocompleteSessionToken.newInstance();
+
+        setSearchBarListener();
+        checkLocationPermissions();
+    }
+
+
+    private void checkLocationPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOC_PERMISSION_REQ_CODE);
+
+            } else {
+                initializeMap();
+            }
+        } else {
+            initializeMap();
+        }
+    }
+
+    private void initializeMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        myGoogleMap = googleMap;
+        myGoogleMap.setMyLocationEnabled(true);
+        myGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
 
-        LatLng sydney = new LatLng(0.034, 33.57);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        checkLocationSettings();
     }
+
+    private void checkLocationSettings() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder locationSettingsBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(MapsActivity.this);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(locationSettingsBuilder.build());
+        task.addOnSuccessListener(MapsActivity.this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                getDeviceLocation();
+
+            }
+        });
+
+        task.addOnFailureListener(MapsActivity.this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                    try {
+                        resolvableApiException.startResolutionForResult(MapsActivity.this, LOC_ENABLED_CHECK_CODE);
+                    } catch (IntentSender.SendIntentException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOC_PERMISSION_REQ_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initializeMap();
+                }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOC_ENABLED_CHECK_CODE) {
+            if (resultCode == RESULT_OK) {
+                getDeviceLocation();
+            }
+        }
+    }
+
+    private void getDeviceLocation() {
+        fusedLocationProviderClient.getLastLocation()
+                .addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                LatLng newLoc = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                                myGoogleMap.addMarker(new MarkerOptions().position(newLoc).title("Marker"));
+                                myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLoc, DEFAULT_ZOOM));
+
+                            } else {
+                                locationRequest = LocationRequest.create();
+                                locationRequest.setInterval(10000);
+                                locationRequest.setFastestInterval(5000);
+                                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                                locationCallback = new LocationCallback() {
+
+                                    @Override
+                                    public void onLocationResult(LocationResult locationResult) {
+                                        super.onLocationResult(locationResult);
+                                        if (locationResult != null) {
+                                            lastKnownLocation = locationResult.getLastLocation();
+                                            Log.d("KNOWNLOCTWO", lastKnownLocation.toString());
+                                            LatLng newLoc = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                                            myGoogleMap.addMarker(new MarkerOptions().position(newLoc).title("Marker"));
+                                            myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLoc, DEFAULT_ZOOM));
+                                            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+                                        }
+                                    }
+                                };
+                                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void setSearchBarListener() {
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text.toString(), true, null, true);
+
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+                if (buttonCode == MaterialSearchBar.BUTTON_NAVIGATION) {
+
+                } else if (buttonCode == MaterialSearchBar.BUTTON_BACK) {
+                    materialSearchBar.disableSearch();
+
+                } else if (buttonCode == MaterialSearchBar.BUTTON_SPEECH) {
+
+                }
+
+            }
+        });
+
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                final FindAutocompletePredictionsRequest predictionRequest = FindAutocompletePredictionsRequest.builder()
+                        .setTypeFilter(TypeFilter.ADDRESS)
+                        .setSessionToken(autocompleteSessionToken)
+                        .setQuery(s.toString())
+                        .build();
+                placesClient.findAutocompletePredictions(predictionRequest)
+                        .addOnCompleteListener(new OnCompleteListener<FindAutocompletePredictionsResponse>() {
+                            @Override
+                            public void onComplete(@NonNull Task<FindAutocompletePredictionsResponse> task) {
+                                if (task.isSuccessful()) {
+                                    FindAutocompletePredictionsResponse predictionResponse = task.getResult();
+                                    if (predictionResponse != null) {
+                                        List<AutocompletePrediction> predictionList = predictionResponse.getAutocompletePredictions();
+                                        if (predictionList.isEmpty()) {
+                                            Log.d("EMPTY", "PRED");
+
+                                        } else {
+                                            suggestionsList = new ArrayList<>();
+                                            for (AutocompletePrediction prediction : predictionList) {
+                                                suggestionsList.add(prediction.getFullText(null).toString());
+                                                Log.d("PRED", prediction.getPlaceId());
+                                                Log.d("PRED2", prediction.getFullText(null).toString());
+
+                                            }
+                                            runOnUiThread(new Runnable() {
+
+                                                @Override
+                                                public void run() {
+                                                    materialSearchBar.updateLastSuggestions(suggestionsList);
+                                                    if (!materialSearchBar.isSuggestionsVisible()) {
+                                                        materialSearchBar.showSuggestionsList();
+
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        });
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
 }
